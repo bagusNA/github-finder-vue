3014599212f9f13dd870ff07a900664c7ea93f6f
Bagus Nur Andiansyah <bagusnur@protonmail.com>
2022-06-11 15:15:30 +0800

    Added 404 card

---
+++ src/components/cards/NotFoundCard.vue
@@ -0,0 +1,8 @@
+<template>
+  <div className="flex bg-card-light p-5 rounded-2xl shadow-2xl lg:p-8 dark:bg-card-dark">
+    <div className="flex-1 py-20 text-center font-bold">
+      <h1 className="text-7xl text-slate-800 dark:text-slate-100">404</h1>
+      <p className="text-slate-400">User not found!</p>
+    </div>
+  </div>
+</template>
